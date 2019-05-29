1255cf2bed70cfc5b4c92407251396e327745c1d
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2019-05-29 12:44:18 -0400

    Calculate the Area of a Triangle

---
+++ Calculating Area (2).py
@@ -0,0 +1,23 @@
+#Arianne Ghislaine Rull
+#ICS2O1 - Mr. Kordbachech
+#April 1st, 2019
+#Calculation of Area
+#Calculate the area of a triangle
+
+print ('Calculate the area of a triangle')
+
+print ('base * height/2')
+#Get input from the user
+base=input ('Enter the base of triangle')
+#Convert the string input to float
+base=float (base)
+
+height=input ('Enter the height of triangle')
+height=float (height)
+print ('base ', base, 'height ', height)
+
+#Use algorithm to calculate the area
+area= (base * height/2)
+
+#output the area of triangle
+print ('the area of a triangle', area)
