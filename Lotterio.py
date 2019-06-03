25998dc205d5f93fa5a416766e2db7b07cee7a72
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2019-06-02 20:29:07 -0400

    Create Lotterio.py

---
+++ Lotterio.py
@@ -0,0 +1,12 @@
+#Lotterio 6/49
+#Arianne Rull
+#ICS2O1 - Mr. Kordbacheh
+#Date: April 8th 2019
+
+import random
+set1=sorted(random.sample(range(1,50), 6))
+print (set1)
+set2=sorted(random.sample(range(1,50), 6))
+print (set2)
+set3=sorted(random.sample(range(1,50), 6))
+print (set3)
